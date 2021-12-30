741026086e571d8ecba9d2d42e309d5b9f5218a7
Evan TeagueMacBook Pro <eteague13@gmail.com>
2021-12-30 11:38:27 -0500

    countdown-timer

---
+++ countdown-timer/components/Social.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Image from "next/image";
+
+function Social(props) {
+    return (
+        <div className="social">
+            <Image className="social__icon" src="/icon-facebook.svg" height={30} width={30} />
+            <Image className="social__icon" src="/icon-pinterest.svg" height={30} width={30} />
+            <Image className="social__icon" src="/icon-instagram.svg" height={30} width={30} />
+            <style>
+                {`
+                    .social {
+                        margin-top: auto;
+                        margin-bottom: 50px;
+                        z-index: 1;
+                        display: flex;
+                        width: 250px;
+                        justify-content: space-evenly;
+                    }
+
+                    .social__icon {
+                        margin: 0 20px;
+                    }
+                `}
+            </style>
+        </div>
+    )
+}
+
+export default Social;
