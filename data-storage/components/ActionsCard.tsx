a2d569fbb6a0aa48dc88bd85ee1ca3d3c3d57ce6
Evan Teague <evanteaguemythical@Evans-Work-MacBook-Pro.local>
2022-01-21 11:43:43 -0500

    fix: build lint issues

--- data-storage/components/ActionsCard.tsx
+++ data-storage/components/ActionsCard.tsx
@@ -6,12 +6,12 @@ export default function ActionsCard() {
     return (
         <div className="bg-darkBlue p-10 rounded-lg-tr-xxl flex flex-col justify-center xl:pr-32">
             <div className="relative aspect-square w-28">
-                <Image src="/logo.svg" layout="fill" objectFit="contain" />
+                <Image src="/logo.svg" alt="Fylo" layout="fill" objectFit="contain" />
             </div>
             <div className="flex">
-                <Logo className="mr-2" src="/icon-document.svg" />
-                <Logo className="mr-2" src="/icon-folder.svg" />
-                <Logo className="mr-2" src="/icon-upload.svg" />
+                <Logo className="mr-2" src="/icon-document.svg" alt="documents" />
+                <Logo className="mr-2" src="/icon-folder.svg" alt="browse" />
+                <Logo className="mr-2" src="/icon-upload.svg" alt="upload" />
             </div>
         </div>
     )
--- data-storage/components/Logo.tsx
+++ data-storage/components/Logo.tsx
@@ -5,14 +5,15 @@ import classNames from 'classnames';
 interface LogoProps {
     src: string;
     className: string;
+    alt: string;
 }
 
 export default function Logo(props: LogoProps) {
-    const { src, className } = props;
+    const { src, className, alt } = props;
     const classes = classNames("bg-veryDarkBlue p-3 rounded-lg flex min-w-[48px]", className);
     return (
         <div className={classes}>
-            <Image src={src} width="24" height="24" />
+            <Image src={src} width="24" height="24" alt={alt} />
         </div>
     )
 }
